4a847994c122ee3d59183f95e8f4160b1dfa8168
akhegade <54054604+akhegade@users.noreply.github.com>
2020-05-15 23:31:22 +0530

    challenge 2
    Adding variable name and lucky number to dom

--- src/index.js
+++ src/index.js
@@ -6,14 +6,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const name = "ANIL HEGDE";
+const luckyNum = 7;
+
 ReactDOM.render(
   <div>
-    <h1>My Favourite Car's</h1>
-    <ul>
-      <li>BMW</li>
-      <li>FERRARI</li>
-      <li>AUDI</li>
-    </ul>
+    <h1>Hello {name}</h1>
+    <p>My lucky number {luckyNum}</p>
   </div>,
   document.getElementById("root")
 );
