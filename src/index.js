9ec7e2d5341e0528eed869ae85825abfb8211dca
akhegade <54054604+akhegade@users.noreply.github.com>
2020-05-16 11:20:54 +0530

    challenge 3
    adding full name with two different variable into jsx

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = "ANIL HEGDE";
+const fName = "ANIL";
+const lName = "Hegde";
 const luckyNum = 7;
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name}</h1>
+    <h1>
+      Hello {fName} {lName}
+    </h1>
     <p>My lucky number {luckyNum}</p>
   </div>,
   document.getElementById("root")
