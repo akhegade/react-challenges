16b8c3dbc2d61bdb9d9d56eafcb4baebdf54927e
akhegade <54054604+akhegade@users.noreply.github.com>
2020-05-16 11:46:54 +0530

    challenge 4
    this creating react app from scratch and adding two para

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 //Create a react app from scratch.
-//It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
 
 import React from "react";
 import ReactDOM from "react-dom";
 
-const fName = "ANIL";
-const lName = "Hegde";
-const luckyNum = 7;
+const myName = "Anil Hegde";
+const copyRightYear = new Date().getFullYear();
 
 ReactDOM.render(
   <div>
-    <h1>
-      Hello {fName} {lName}
-    </h1>
-    <p>My lucky number {luckyNum}</p>
+    <p>Created by {myName}</p>
+    <p>Copyright {copyRightYear}</p>
   </div>,
   document.getElementById("root")
 );
